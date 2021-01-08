0b4ab1e2328f048d83f48553e805548c8b44e246
RaviC Rachamalla <rach0022@algonquinlive.com>
2021-01-08 16:01:46 -0500

    updated SavedPlants to be unique_together

--- search/models.py
+++ search/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class SavedPlants(models.Model):
-    plant_id = models.IntegerField(unique=True) # duplicate plants should not be saved
+    plant_id = models.IntegerField() # duplicate plants should not be saved
     user = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('user', 'plant_id')) # to stop users from duplicate saves
